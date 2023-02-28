63562610c15ba87a9c384376b51e65b2883518ec
Iamdevopstrainer <danuka19980@gmail.com>
2023-02-28 21:42:42 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+
+# Give the python file some time before connecting
+time.sleep(3)
+
+#Mouse Function
+
+# Print the resulution of your screen
+#print(pyautogui.size())
+
+
+# Prints the Current Position
+#print(pyautogui.position())
+
+# Moves the mouse overtime
+pyautogui.moveTo(100, 100, 3)
+
+
+
+
